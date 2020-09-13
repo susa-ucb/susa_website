5881246767cedf3a949ba40141343d741edcfbb6
Edward Chang <edwardchang116@berkeley.edu>
2020-09-13 13:32:12 -0700

    add some comments to db_init

--- db_init.py
+++ db_init.py
@@ -1,9 +1,12 @@
+# This script drops all tables then recreates them
+# DO NOT run this somehow run this if you are looking to just modify one tables
+# Manually drop a table through sqlite3 then modify this to only use .create_all()
+
 if __name__ == '__main__':
     try:
         from susa_website import db
 
         db.drop_all()
-
         db.create_all()
 
     except ImportError:
