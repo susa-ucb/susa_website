680ec574a36a2094102342ff26a85f780576c349
Edward Chang <edwardchang116@berkeley.edu>
2020-09-14 17:25:15 -0700

    comment out create_all

--- db_init.py
+++ db_init.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
         from susa_website import db
 
         #db.drop_all()
-        db.create_all()
+        #db.create_all()
 
     except ImportError:
         print("Something went wrong when importing the Flask app.\nYou probably didn't activate your venv.")
