d951840b3d239759e5450f496a167b217ef30287
Edward Chang <equilnite@gmail.com>
2020-11-01 18:16:57 -0800

    map size increase; repositioning

--- susa_website/static/scripts/course_map/course_map.js
+++ susa_website/static/scripts/course_map/course_map.js
@@ -16,7 +16,7 @@ function mag(x1, y1, x2, y2) {
 function scaleSegment(x1, y1, x2, y2) {
   const magn = mag(x1, y1, x2, y2);
   const prop1 = (magn - 35) / magn;
-  const prop2 = (magn - 50) / magn;
+  const prop2 = (magn - 45) / magn;
   const x_1p = x2 - (x2 - x1) * prop1;
   const y_1p = y2 - (y2 - y1) * prop1;
   const x_2p = x1 + (x2 - x1) * prop2;
@@ -32,7 +32,7 @@ class Node {
 
   getLabel() { return this.label; }
   getX() { return this.position[0]+200; }
-  getY() { return this.position[1]+100; }
+  getY() { return this.position[1]+150; }
   getID() { return this.getLabel().split(' ').join(''); }
 
   highlight(color) {
@@ -290,7 +290,7 @@ function circle_gen(center, radius) {
   return circle_coord
 }
 
-var department_circ = circle_gen([700, 450], 45)
+var department_circ = circle_gen([850, 250], 45);
 
 var deps = [
   new Department("Statistics", department_circ(1/2), "Pinkish"),
@@ -298,49 +298,56 @@ var deps = [
   new Department("Math", department_circ(-1/6), "GrassGreen")
 ]
 
+var cat_circ = circle_gen([850, 425], 80);
+
 var cats = [
-  new Category("Lower Div Req", [0, 600]),
-  new Category("Core Class", [100, 600]),
-  new Category("Elective", [200, 600]),
-  new Category("Programming", [300, 600]),
+  new Category("Lower Div Req", cat_circ(1/4)),
+  new Category("Core Class", cat_circ(3/4)),
+  new Category("Elective", cat_circ(5/4)),
+  new Category("Programming", cat_circ(7/4))
 ]
 
 var stat_135_circ = circle_gen([350, 300], 200)
 var stat_135_circ2 = circle_gen([350, 300], 125)
 var stat_134_circ = circle_gen([55, 250], 150)
-var math_1b_circ = circle_gen([150, 0], 175)
-var elective_circ = circle_gen([500, 150], 300)
+var math_1a_circ = circle_gen([0, 0], 125)
+var math_1a_circ2 = circle_gen([0, 0], 250)
+var math_1b_circ = circle_gen(math_1a_circ(-3/8), 125)
+var math_1b_circ2 = circle_gen(math_1a_circ(-3/8), 250)
+var data_c8_circ = circle_gen([425, 50], 175)
+var data_c100_circ = circle_gen(data_c8_circ(-7/16), 175)
+var elective_circ = circle_gen([400, 75], 600)
 
 var els = [
-  new Element("1A", [], ["131A", "1B", "20", "88"], [], [0, 0], ["Lower Div Req"], "Math"),
-  new Element("1B", ["1A"], ["89A","53", "54", "55", "134", "C140"], ["20"], [150, 0], ["Lower Div Req"], "Math"),
-  new Element("53", ["1B"], ["134", "C140", "154"], ["134", "C140"], math_1b_circ(10/8), ["Lower Div Req"], "Math"),
-  new Element("54", ["1B"], ["135", "C100", "C102", "C140"], [], math_1b_circ(12/8), ["Lower Div Req"], "Math"),
-  new Element("55", ["1B"], ["154"], ["154"], [0, 50], [], "Math"),
-  new Element("20", ["1A"], ["131A", "C140"], [], [300,0], ["Lower Div Req"], "Statistics"),
-  new Element("88", ["1A", "C8"], ["C8"], [], [200, 75], [], "Statistics"),
-  new Element("89A", ["1B", "C8"], ["135", "C100", "C102", "C140", "C8"], ["C8"], math_1b_circ(15.5/8), [], "Statistics"),
-  new Element("131A", ["33A", "133", "C8", "20", "1A"], ["33A", "133"], ["33A", "133"], [575, 150], [], "Data"),
-  new Element("133", [], ["131A", "159", "151A", "152", "153", "158", "135"], ["131A", "158", "152", "151A", "153","135"], [750, 200], ["Programming", "Core Class"], "Statistics"),
-  new Element("134", ["53", "1B"], ["135", "150", "155", "C102", "152", "153"], [], [55, 250], ["Lower Div Req", "Core Class"], "Statistics"),
-  new Element("135", ["54", "89A", "134", "C140", "133"], ["151A", "152", "153", "154", "156", "157", "158", "159", "133"], ["152", "153", "133"], [350, 300], ["Lower Div Req", "Core Class"], "Statistics"),
-  new Element("150", ["134", "C140"], [], [], stat_134_circ(10.5/8), ["Elective"], "Statistics"),
-  new Element("151A", ["135", "C102", "133"], [], [], stat_135_circ(8/8), ["Elective"], "Statistics"),
-  new Element("152", ["135", "133", "134"], [], [], stat_135_circ2(9.5/8), ["Elective"], "Statistics"),
-  new Element("153", ["134", "133", "135"], [], [], stat_135_circ(11/8), ["Elective"], "Statistics"),
-  new Element("154", ["135", "53", "55"], [], [], stat_135_circ2(12.5/8), ["Elective"], "Statistics"),
-  new Element("155", ["134", "C140"], [], [],  stat_134_circ(12.25/8), ["Elective"], "Statistics"),
-  new Element("156", ["135"], [], [], stat_135_circ(14/8), ["Elective"], "Statistics"),
-  new Element("157", ["135"], [], [], [400, 450], ["Elective"], "Statistics"),
-  new Element("158", ["135", "133"], ["135"], [], stat_135_circ2(15.5/8), ["Elective"], "Statistics"),
-  new Element("159", ["135", "133"], [], [], stat_135_circ(15.5/8), ["Elective"], "Statistics"),
-  new Element("33A", [], ["131A"], ["131A"], [750, 0], ["Programming"], "Statistics"),
+  new Element("1A", [], ["C131A", "1B", "20", "88"], [], [0, 0], ["Lower Div Req"], "Math"),
+  new Element("1B", ["1A"], ["89A","53", "54", "55", "134", "C140"], ["20"], math_1a_circ(-3/8), ["Lower Div Req"], "Math"),
+  new Element("53", ["1B"], ["134", "C140", "154"], ["134", "C140"], math_1b_circ(13/8), ["Lower Div Req"], "Math"),
+  new Element("54", ["1B"], ["135", "C100", "C102", "C140"], [], math_1b_circ(15/8), ["Lower Div Req"], "Math"),
+  new Element("55", ["1B"], ["154"], ["154"], math_1b_circ(11/8), [], "Math"),
+  new Element("C8", [], ["C131A", "89A","C100", "C140", "88"], [], [425, 25], ["Lower Div Req"], "Data"),
+  new Element("20", ["1A"], ["C131A", "C140"], [], math_1a_circ2(0), ["Lower Div Req"], "Statistics"),
+  new Element("33A", [], ["C131A"], ["C131A"], [750, 0], ["Programming"], "Statistics"),
   new Element("33B", ["61A"], [], [], [750, 100], ["Programming", "Core Class"], "Statistics"),
   new Element("61A", [], ["C100", "33B", "C140"], [], [600, 0], ["Programming"]),
-  new Element("C8", [], ["131A", "89A","C100", "C140", "88"], [], [450, 0], ["Lower Div Req"], "Data"),
-  new Element("C100", ["61A", "C8", "54", "89A"], ["C102", "C140", "54", "89A"], [], [525, 100], ["Core Class"], "Data"),
-  new Element("C102", ["C100", "134", "54", "C140", "89A"], ["151A"], [], [450, 200], ["Elective"], "Data"),
-  new Element("C140", ["54", "53", "C8", "C100", "20", "61A", "89A", "1B"], ["135", "150", "155", "C102", "54", "89A"], [], [315, 175], ["Lower Div Req", "Core Class"], "Data")
+  new Element("88", ["1A", "C8"], ["C8"], [], data_c8_circ(5/4), [], "Statistics"),
+  new Element("89A", ["1B", "C8"], ["135", "C100", "C102", "C140", "C8"], ["C8"], math_1b_circ(17/8), [], "Statistics"),
+  new Element("C100", ["61A", "C8", "54", "89A"], ["C102", "C140", "54", "89A"], [], data_c8_circ(-7/16), ["Core Class"], "Data"),
+  new Element("C102", ["C100", "134", "54", "C140", "89A"], ["151A"], [], data_c100_circ(12.5/8), ["Elective"], "Data"),
+  new Element("C131A", ["33A", "133", "C8", "20", "1A"], ["33A", "133"], ["33A", "133"], data_c8_circ(-3/16), [], "Data"),
+  new Element("133", [], ["C131A", "159", "151A", "152", "153", "158", "135"], ["C131A", "158", "152", "151A", "153","135"], [525, 500], ["Programming", "Core Class"], "Statistics"),
+  new Element("134", ["53", "1B"], ["135", "150", "155", "C102", "152", "153"], [], math_1b_circ2(12/8), ["Lower Div Req", "Core Class"], "Statistics"),
+  new Element("135", ["54", "89A", "134", "C140", "133"], ["151A", "152", "153", "154", "156", "157", "158", "159", "133"], ["152", "153", "133"], [325, 500], ["Lower Div Req", "Core Class"], "Statistics"),
+  new Element("C140", ["54", "53", "C8", "C100", "20", "61A", "89A", "1B"], ["135", "150", "155", "C102", "54", "89A"], [], data_c100_circ(9.5/8), ["Lower Div Req", "Core Class"], "Data"),
+  new Element("150", ["134", "C140"], [], [], elective_circ(9.7/8), ["Elective"], "Statistics"),
+  new Element("151A", ["135", "C102", "133"], [], [], elective_circ(10.5/8), ["Elective"], "Statistics"),
+  new Element("152", ["135", "133", "134"], [], [], elective_circ(10.9/8), ["Elective"], "Statistics"),
+  new Element("153", ["134", "133", "135"], [], [], elective_circ(11.3/8), ["Elective"], "Statistics"),
+  new Element("154", ["135", "53", "55"], [], [], elective_circ(11.7/8), ["Elective"], "Statistics"),
+  new Element("155", ["134", "C140"], [], [], elective_circ(10.1/8), ["Elective"], "Statistics"),
+  new Element("156", ["135"], [], [], elective_circ(12.1/8), ["Elective"], "Statistics"),
+  new Element("157", ["135"], [], [], elective_circ(12.5/8), ["Elective"], "Statistics"),
+  new Element("158", ["135", "133"], ["135"], [], elective_circ(12.9/8), ["Elective"], "Statistics"),
+  new Element("159", ["135", "133"], [], [], elective_circ(13.3/8), ["Elective"], "Statistics")
 ]
 
 let div = document.getElementById("courses");
@@ -354,10 +361,12 @@ for (cat in cats) {
 }
 
 for (el in els) {
-  var el = els[el];
-  var edges = el.genEdges(els)
-  div.innerHTML += el.genNode();
+  var edges = els[el].genEdges(els)
   for (edge in edges) {
     div.innerHTML += edges[edge];
   }
+}
+
+for (el in els) {
+  div.innerHTML += els[el].genNode();
 }
