e89ef472a090f712db3b0efd17fe245ea39f06f1
Edward Chang <equilnite@gmail.com>
2020-12-21 14:07:26 -0800

    database table creation for catalogue

--- db_init.py
+++ db_init.py
@@ -5,9 +5,12 @@
 if __name__ == '__main__':
     try:
         from susa_website import db
+        from scrape_catalogue import course_df
 
-        #db.drop_all()
-        #db.create_all()
+        # db.drop_all()
+        # db.create_all()
+        # course_df.to_sql(name='catalogue', con=db.engine, index=False)
+
 
     except ImportError:
         print("Something went wrong when importing the Flask app.\nYou probably didn't activate your venv.")
--- scrape_catalogue.py
+++ scrape_catalogue.py
@@ -1,11 +1,10 @@
-from pathlib import Path
+# for getting webcontent
 from requests import get
-
 from bs4 import BeautifulSoup
-
+# convenience of data structuring
 from collections import namedtuple
-
-import pandas as pd
+# for data frame construction
+from pandas import DataFrame
 
 Course = namedtuple('Course', 'id code title units desc details')
 
@@ -20,7 +19,6 @@ def add_courses(department):
 
     for courseblock in courseblocks:
         code = courseblock.find(attrs = {'class': ('code')}).text.replace('\xa0', ' ')
-        print(code)
         num = code.split()[1]
         title = courseblock.find(attrs = {'class': ('title')}).text
         units = courseblock.find(attrs = {'class': ('hours')}).text
@@ -31,11 +29,13 @@ def add_courses(department):
         if desc2_in:
             desc += courseblock.find(attrs = {'class': ('deschide')}).text
         details = courseblock.find_all(attrs = {'class': 'course-section'})
+        details = ''.join([str(detail) for detail in details])
         courses[num] = Course(num, code, title, units, desc, details)
+        # break it just so that we don't do more than neccessary
         if code == 'STAT 159' or code == 'MATH 55' or code == 'COMPSCI 61A':
             break
 
 for department in ['compsci', 'math', 'stat']:
     add_courses(department)
 
-course_df = pd.DataFrame.from_dict(data=courses, orient='index')
+course_df = DataFrame.from_dict(data=courses, orient='index')
